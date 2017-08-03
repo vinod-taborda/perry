b1ffbfc1387ab874a88071207aafd9c9d2aa499a
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-08-03 15:04:24 -0700

    access token is not used in JWT

--- src/main/java/gov/ca/cwds/service/OauthLoginService.java
+++ src/main/java/gov/ca/cwds/service/OauthLoginService.java
@@ -5,10 +5,10 @@ import gov.ca.cwds.security.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.UUID;
 
 /**
  * Created by dmitry.rudenko on 5/22/2017.
@@ -21,14 +21,11 @@ public class OauthLoginService implements LoginService {
     IdentityMappingService identityMappingService;
     @Autowired
     JwtService jwtService;
-    @Autowired
-    OAuth2ClientContext oauth2ClientContext;
 
     public String login(String providerId) throws Exception {
         UniversalUserToken userToken = (UniversalUserToken) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String accessToken = oauth2ClientContext.getAccessToken().getValue();
         String jwtIdentity = identityMappingService.map(userToken, providerId);
-        return jwtService.generate(accessToken, userToken.getUserId(), jwtIdentity);
+        return jwtService.generate(UUID.randomUUID().toString(), userToken.getUserId(), jwtIdentity);
     }
 
     public String validate(String token) throws Exception {
