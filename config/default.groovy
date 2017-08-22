054686cfd018a9dd2dbeedd5c8714bae56c847ab
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-08-22 15:17:38 -0700

    intake identity mappping

--- config/default.groovy
+++ config/default.groovy
@@ -1,2 +1,24 @@
-[user: authorization.userId, roles: ["none"], staffId: authorization.staffPersonId]
+privileges = []
+authorization.authorityPrivilege.findAll {
+    it.authPrivilegeCode == "P" && it.endDate == null
+
+} each {
+    privileges.push it.authPrivilegeTypeDesc
+}
+
+def supervisorAuthorities = ["S", "A", "T", "B"]
+
+def supervisor = authorization.unitAuthority != null && authorization.unitAuthority.size() > 0 && authorization.unitAuthority.every { a ->
+    supervisorAuthorities.any {
+        it == a.unitAuthorityCode
+    }
+}
+
+
+[user       : authorization.userId,
+ roles      : [supervisor ? "Supervisor" : "SocialWorker"],
+ staffId    : authorization.staffPersonId,
+ county_name: authorization.county,
+ county_code: authorization.staffPerson.countyCode,
+ privileges : privileges]
 
