0f571b6c97aee89416dcaf2a708dc94d1d6713fb
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-08-24 12:10:19 -0700

    basic smoke test

---
+++ src/main/java/gov/ca/cwds/smoketest/JwtCheck.java
@@ -0,0 +1,30 @@
+package gov.ca.cwds.smoketest;
+
+import gov.ca.cwds.security.jwt.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtCheck implements HealthIndicator {
+  private final static String TEST_IDENTITY = "identity";
+
+  @Autowired
+  JwtService jwtService;
+
+  @Override
+  public Health health() {
+    try {
+      String jwt = jwtService.generate("id", "subject", TEST_IDENTITY);
+      String jwtContent = jwtService.validate(jwt);
+      if(!TEST_IDENTITY.equals(jwtContent)) {
+        return Health.down().withDetail("message", "Token validation failed").build();
+      }
+      return Health.up().build();
+    } catch (Exception e) {
+      return Health.down().withException(e).build();
+    }
+  }
+}
