7f29e72da9bfd4229da5b67fce008817861f52a9
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-10-12 14:34:54 -0700

    smoke test fixed

--- src/test/java/gov/ca/cwds/smoketest/SmokeTest.java
+++ src/test/java/gov/ca/cwds/smoketest/SmokeTest.java
@@ -10,16 +10,30 @@ import java.util.Map;
  * Created by dmitry.rudenko on 8/24/2017.
  */
 public class SmokeTest {
-
+  private long RETRY_PERIOD = 5 * 1000 * 60;//5 minutes
+  private long RETRY_TIMEOUT = 20 * 1000; //20 seconds;
 
   @Test
   public void test() throws Exception {
     RestTemplate restTemplate = new RestTemplate();
     ObjectMapper mapper = new ObjectMapper();
-    String result = restTemplate.getForObject(System.getProperty("perry.health.check.url"), String.class);
+    String result = getHealth(restTemplate);
     Map map = mapper.readValue(result, Map.class);
     if ("DOWN".equals(map.get("status"))) {
       throw new Exception(result);
     }
+  }
+
+  private String getHealth(RestTemplate restTemplate) throws Exception {
+    String url = System.getProperty("perry.health.check.url");
+    long endTime = System.currentTimeMillis() + RETRY_PERIOD;
+    while (System.currentTimeMillis() < endTime) {
+      try {
+        return restTemplate.getForObject(url, String.class);
+      } catch (Exception e) {
+        Thread.sleep(RETRY_TIMEOUT);
+      }
+    }
+    throw new Exception("CAN'T CONNECT TO: " + url);
   }
 }
