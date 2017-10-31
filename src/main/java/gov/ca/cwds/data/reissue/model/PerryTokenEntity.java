82f6bbf960e1e8195504fa60a57595ce58890b46
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-10-31 12:40:57 -0700

    db

--- src/main/java/gov/ca/cwds/data/reissue/model/PerryTokenEntity.java
+++ src/main/java/gov/ca/cwds/data/reissue/model/PerryTokenEntity.java
@@ -20,8 +20,6 @@ public class PerryTokenEntity implements Serializable {
   private String accessCode;
   @Column(name = "user_id")
   private String user;
-  //@Lob
-
   @Column(name = "access_token", length = 20000)
   private byte[] accessToken;
   @Temporal(TemporalType.TIMESTAMP)
