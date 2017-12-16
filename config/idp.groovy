06489cc68e06c83959949f82eb73f1496815a71c
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-12-15 16:42:37 -0800

    redis serialization fixed

--- config/idp.groovy
+++ config/idp.groovy
@@ -3,7 +3,7 @@ import groovy.json.JsonSlurper
 universalUserToken.userId = idpToken["safid.racfid"]?.toUpperCase()
 def parser = new JsonSlurper()
 try {
-    universalUserToken.roles = parser.parseText(parser.parseText(idpToken.basicprofile).User_Properties.Roles).Selections.keySet()
+    universalUserToken.roles = parser.parseText(parser.parseText(idpToken.basicprofile).User_Properties.Roles).Selections.keySet() as HashSet
 }catch (all) {
     all.printStackTrace()
 }
