a707fd4719952a926b7d916b30edac206cefed00
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2017-12-27 14:36:09 -0800

    idp mapping fixed

--- config/idp.groovy
+++ config/idp.groovy
@@ -9,3 +9,7 @@ def parse = { text ->
 universalUserToken.userId = idpToken["safid.racfid"]?.toUpperCase()
 
 universalUserToken.roles = parse(parse(idpToken.basicprofile)?.User_Properties?.Roles)?.Selections?.keySet() as HashSet
+if(!universalUserToken.roles) {
+    universalUserToken.roles = new HashSet<>()
+    println "INFO: There are no IDP roles provided"
+}
